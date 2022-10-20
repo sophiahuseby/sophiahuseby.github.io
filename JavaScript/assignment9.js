260d00b4d799380dbad3a5497e473ec17832cf4e
sophiahuseby <sophia.huseby@colorado.edu>
2022-10-20 16:01:31 -0600

    update

--- JavaScript/assignment9.js
+++ JavaScript/assignment9.js
@@ -6,26 +6,27 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-var fileNames = [];
-fileNames.push("/img/pic1.jpg");
-fileNames.push("/img/pic2.jpg");
-fileNames.push("/img/pic3.jpg");
-fileNames.push("/img/pic4.jpg");
-fileNames.push("/img/pic5.jpg");
+const pictures = ['pic1.jpg','pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
 
-<img src="/img/pic1.jpg" alt="" />
-<img src="/img/pic2.jpg" alt="" />
-<img src="/img/pic3.jpg" alt="" />
-<img src="/img/pic4.jpg" alt="" />
-<img src="/img/pic5.jpg" alt="" />
+const altText = {
+    'pic1.jpg' : "human eye closeup",
+    'pic2.jpg' : "rocky landform",
+    'pic3.jpg' : "purple flowers",
+    'pic4.jpg' : "egyptian paintings",
+    'pic5.jpg' : "butterfly"
+}
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-
+for(const picture of pictures){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/${image}');
+    newImage.setAttribute('alt', altText[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e=> {
+        displayedImage.src = e.target.src;
+        displayedImage.alt = e.target.src;
+    })
+}
 /* Wiring up the Darken/Lighten button */
