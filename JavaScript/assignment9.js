d3bbd6d7bc2f53d4893fcd53080265a54f1ff09d
sophiahuseby <sophia.huseby@colorado.edu>
2022-10-20 15:45:50 -0600

    assignment9 updates

---
+++ JavaScript/assignment9.js
@@ -0,0 +1,31 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+
+var fileNames = [];
+fileNames.push("/img/pic1.jpg");
+fileNames.push("/img/pic2.jpg");
+fileNames.push("/img/pic3.jpg");
+fileNames.push("/img/pic4.jpg");
+fileNames.push("/img/pic5.jpg");
+
+/* Declaring the alternative text for each image file */
+
+<img src="/img/pic1.jpg" alt="" />
+<img src="/img/pic2.jpg" alt="" />
+<img src="/img/pic3.jpg" alt="" />
+<img src="/img/pic4.jpg" alt="" />
+<img src="/img/pic5.jpg" alt="" />
+
+/* Looping through images */
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
---
+++ assignments/assignment9.html
@@ -0,0 +1,12 @@
+<h1>Image gallery example</h1>
+
+<div class="full-img">
+  <img
+    class="displayed-img"
+    src="images/pic1.jpg"
+    alt="Closeup of a blue human eye" />
+  <div class="overlay"></div>
+  <button class="dark">Darken</button>
+</div>
+
+<div class="thumb-bar"></div>
