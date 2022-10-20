048e1ce49dc1b027b0a6ff2d52ef3a20948de6fd
sophiahuseby <sophia.huseby@colorado.edu>
2022-10-20 16:16:02 -0600

    Update assignment9.js

--- JavaScript/assignment9.js
+++ JavaScript/assignment9.js
@@ -6,7 +6,7 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-const pictures = ['../../img/pic1.jpg','../../img/pic2.jpg', '../../img/pic3.jpg', '../../img/pic4.jpg', '../../img/pic5.jpg'];
+const images = ['../../img/pic1.jpg','../../img/pic2.jpg', '../../img/pic3.jpg', '../../img/pic4.jpg', '../../img/pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
 
@@ -19,14 +19,27 @@ const altText = {
 }
 
 /* Looping through images */
-for(const picture of pictures){
+for (const image of images) {
     const newImage = document.createElement('img');
-    newImage.setAttribute('src', 'images/${image}');
-    newImage.setAttribute('alt', altText[image]);
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', alts[image]);
     thumbBar.appendChild(newImage);
-    newImage.addEventListener('click', e=> {
-        displayedImage.src = e.target.src;
-        displayedImage.alt = e.target.src;
-    })
-}
+    newImage.addEventListener('click', e => {
+      displayedImage.src = e.target.src;
+      displayedImage.alt = e.target.alt;
+    });
+  }
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+  });
