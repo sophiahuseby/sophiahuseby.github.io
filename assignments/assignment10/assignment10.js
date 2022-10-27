0114d66fa33e067ae0d74a9c0aa8cd0cac6208e9
sophiahuseby <sophia.huseby@colorado.edu>
2022-10-26 22:27:00 -0600

    update

---
+++ assignments/assignment10/assignment10.js
@@ -0,0 +1,110 @@
+// problem 1 
+
+[
+    {
+        "name" : "Sam",
+        "department" : "Tech",
+        "designation" : "Manager",
+        "salary" : "400000",
+        "raise eligible" : "true"
+    },
+
+
+    {
+        "name" : "Mary",
+        "department" : "Finance",
+        "designation" : "Trainee",
+        "salary" : "18500",
+        "raise eligible" : "true"
+    },
+
+    {
+        "name" : "Bill",
+        "department" : "HR",
+        "designation" : "Executive",
+        "salary" : "21200",
+        "raise eligible" : "false"
+    }
+]
+
+// problem 2
+
+[
+    {
+        "companyName" : "Tech Stars",
+        "website" : "www.techstars.site",
+        "employees" : [
+            {
+                "name" : "Sam",
+                "department" : "Tech",
+                "designation" : "Manager",
+                "salary" : "400000",
+                "raise eligible" : "true"
+            },
+        
+        
+            {
+                "name" : "Mary",
+                "department" : "Finance",
+                "designation" : "Trainee",
+                "salary" : "18500",
+                "raise eligible" : "true"
+            },
+        
+            {
+                "name" : "Bill",
+                "department" : "HR",
+                "designation" : "Executive",
+                "salary" : "21200",
+                "raise eligible" : "false"
+            }
+        ]
+    }
+]
+
+
+// problem 3
+
+[
+    {
+        "companyName" : "Tech Stars",
+        "website" : "www.techstars.site",
+        "employees" : [
+            {
+                "name" : "Sam",
+                "department" : "Tech",
+                "designation" : "Manager",
+                "salary" : "400000",
+                "raise eligible" : "true"
+            },
+        
+        
+            {
+                "name" : "Mary",
+                "department" : "Finance",
+                "designation" : "Trainee",
+                "salary" : "18500",
+                "raise eligible" : "true"
+            },
+        
+            {
+                "name" : "Bill",
+                "department" : "HR",
+                "designation" : "Executive",
+                "salary" : "21200",
+                "raise eligible" : "false"
+            },
+
+            {
+                "name" : "Anna",
+                "department" : "Tech",
+                "designation" : "HR",
+                "salary" : "25600",
+                "raise eligible" : "false"
+            }
+        ]
+    }
+]
+
+// problem 4
+
