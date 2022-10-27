113f7ecbd0f748b14f98f43065be56da4a1f9083
sophiahuseby <sophia.huseby@colorado.edu>
2022-10-27 16:03:50 -0600

    updates

--- assignments/assignment10/assignment10.js
+++ assignments/assignment10/assignment10.js
@@ -1,110 +1,74 @@
-// problem 1 
+const employees = [
 
-[
-    {
-        "name" : "Sam",
-        "department" : "Tech",
-        "designation" : "Manager",
-        "salary" : "400000",
-        "raise eligible" : "true"
-    },
+        {
+            name : "Sam",
+            department : "Tech",
+            designation : "Manager",
+            salary : "400000",
+            raise : "true"
+        },
+    
+        {
+            name : "Mary",
+            department : "Finance",
+            designation : "Trainee",
+            salary : "18500",
+            raise : "true"
+        },
+    
+        {
+            name : "Bill",
+            department : "HR",
+            designation : "Executive",
+            salary : "21200",
+            raise : "false"
+        }
+    ];
+
+console.log("Problem 1", [Sam, Mary, Bill]);
+
+const company = {
+    companyName : "Tech Stars",
+    website : "www.techstars.site",
+    employees : [Sam, Mary, Bill],
+};
+
+console.log("Problem 2", company);
 
 
-    {
-        "name" : "Mary",
-        "department" : "Finance",
-        "designation" : "Trainee",
-        "salary" : "18500",
-        "raise eligible" : "true"
-    },
+company.employees.push({
+    name : "Anna",
+    department : "Tech",
+    designation : "HR",
+    salary : "25600",
+    raise : "false"
+});
 
-    {
-        "name" : "Bill",
-        "department" : "HR",
-        "designation" : "Executive",
-        "salary" : "21200",
-        "raise eligible" : "false"
+console.log("Problem 3", company);
+
+
+let sum = 0;
+for (let i = 0; i < company.employees.length; i ++){
+    sum += company.employees[i].salary;
+}
+
+console.log("Problem 4", sum);
+
+
+for (let i = 0; i < company.employees.length; i ++){
+    if(company.employees[i].raise){
+        company.employees[i].salary *= 1.1;
+        company.employees[i].raise = false;
     }
-]
+}
 
-// problem 2
-
-[
-    {
-        "companyName" : "Tech Stars",
-        "website" : "www.techstars.site",
-        "employees" : [
-            {
-                "name" : "Sam",
-                "department" : "Tech",
-                "designation" : "Manager",
-                "salary" : "400000",
-                "raise eligible" : "true"
-            },
-        
-        
-            {
-                "name" : "Mary",
-                "department" : "Finance",
-                "designation" : "Trainee",
-                "salary" : "18500",
-                "raise eligible" : "true"
-            },
-        
-            {
-                "name" : "Bill",
-                "department" : "HR",
-                "designation" : "Executive",
-                "salary" : "21200",
-                "raise eligible" : "false"
-            }
-        ]
-    }
-]
+console.log("Problem 5", company);
 
 
-// problem 3
+const workHomeEmployees = ['Anna', 'Sam'];
 
-[
-    {
-        "companyName" : "Tech Stars",
-        "website" : "www.techstars.site",
-        "employees" : [
-            {
-                "name" : "Sam",
-                "department" : "Tech",
-                "designation" : "Manager",
-                "salary" : "400000",
-                "raise eligible" : "true"
-            },
-        
-        
-            {
-                "name" : "Mary",
-                "department" : "Finance",
-                "designation" : "Trainee",
-                "salary" : "18500",
-                "raise eligible" : "true"
-            },
-        
-            {
-                "name" : "Bill",
-                "department" : "HR",
-                "designation" : "Executive",
-                "salary" : "21200",
-                "raise eligible" : "false"
-            },
+for (var i = 0; i < company.employees.length; i ++){
+    company.employees[i].workHome = workHomeEmployees.includes(company.employees[i].name);
+}
 
-            {
-                "name" : "Anna",
-                "department" : "Tech",
-                "designation" : "HR",
-                "salary" : "25600",
-                "raise eligible" : "false"
-            }
-        ]
-    }
-]
-
-// problem 4
-
+console.log("Problem 6", company);
