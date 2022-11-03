b590e40b1477f81901ac63c040ecbfc4a10e6177
sophiahuseby <sophia.huseby@colorado.edu>
2022-11-02 23:59:31 -0600

    Update ica11.js

---
+++ assignments/ica11/ica11.js
@@ -0,0 +1,28 @@
+
+const quoteButton = document.querySelector('.new-quote');
+quoteButton.addEventListener('click', getQuote);
+
+const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
+
+async function getQuote() {
+    console.log("test button click")
+    let text = await fetch(endpoint);
+    let response = await text.text();
+    
+    let json_response = JSON.parse(response);
+
+    console.log(json_response['message']);
+
+    displayQuote(json_response['message']);
+
+}
+
+function displayQuote(x) {
+    // const quoteBox = document.querySelector('#js-quote-text');
+    // const textMessage = document.createTextNode(x);
+    // quoteBox.appendChild(textMessage);
+
+    document.getElementById('js-quote-text').textContent = x;
+}
+
+getQuote();
