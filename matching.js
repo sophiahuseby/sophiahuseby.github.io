61294ce8683216e397e32d8b88e2515fc960345a
sophiahuseby <sophia.huseby@colorado.edu>
2022-12-10 14:43:55 -0700

    update

--- matching.js
+++
@@ -1,6 +0,0 @@
-function unhide() {
-    var show = document.getElementsByClassName("exp");
-    if(show[0].offsetWidth > 0 && show[0].offsetHeight > 0) {
-        show[0].style.visibility = "visible";
-    }
-}
